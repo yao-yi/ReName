9073e6d1ebe3d7aaea472ae8b2ca69a2d8067e2f
Sergey Minav <s@minav.ru>
2016-10-24 17:21:53 +0300

    Добавьте файлы проекта.

---
+++ ReName/FormMain.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace ReName
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.btnChose = new System.Windows.Forms.Button();
+            this.tbOldName = new System.Windows.Forms.TextBox();
+            this.tbNewName = new System.Windows.Forms.TextBox();
+            this.btnRename = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // btnChose
+            // 
+            this.btnChose.Location = new System.Drawing.Point(12, 13);
+            this.btnChose.Name = "btnChose";
+            this.btnChose.Size = new System.Drawing.Size(75, 23);
+            this.btnChose.TabIndex = 0;
+            this.btnChose.Text = "Choose";
+            this.btnChose.UseVisualStyleBackColor = true;
+            this.btnChose.Click += new System.EventHandler(this.btnChose_Click);
+            // 
+            // tbOldName
+            // 
+            this.tbOldName.Location = new System.Drawing.Point(12, 42);
+            this.tbOldName.Name = "tbOldName";
+            this.tbOldName.ReadOnly = true;
+            this.tbOldName.Size = new System.Drawing.Size(197, 20);
+            this.tbOldName.TabIndex = 1;
+            // 
+            // tbNewName
+            // 
+            this.tbNewName.Location = new System.Drawing.Point(12, 68);
+            this.tbNewName.Name = "tbNewName";
+            this.tbNewName.Size = new System.Drawing.Size(197, 20);
+            this.tbNewName.TabIndex = 2;
+            this.tbNewName.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbNewName_KeyPress);
+            // 
+            // btnRename
+            // 
+            this.btnRename.Enabled = false;
+            this.btnRename.Location = new System.Drawing.Point(12, 94);
+            this.btnRename.Name = "btnRename";
+            this.btnRename.Size = new System.Drawing.Size(75, 23);
+            this.btnRename.TabIndex = 3;
+            this.btnRename.Text = "ReName";
+            this.btnRename.UseVisualStyleBackColor = true;
+            this.btnRename.Click += new System.EventHandler(this.btnRename_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.Filter = "|*.sln";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(220, 133);
+            this.Controls.Add(this.btnRename);
+            this.Controls.Add(this.tbNewName);
+            this.Controls.Add(this.tbOldName);
+            this.Controls.Add(this.btnChose);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ReName";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnChose;
+        private System.Windows.Forms.TextBox tbOldName;
+        private System.Windows.Forms.TextBox tbNewName;
+        private System.Windows.Forms.Button btnRename;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+    }
+}
+
---
+++ ReName/FormMain.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReName
+{
+    
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+        FolderBrowserDialog fbd = new FolderBrowserDialog();
+        StringBuilder st = new StringBuilder();
+        DialogResult result;
+        DirectoryInfo p;
+        FileInfo info;
+        string[] s;
+        private void btnChose_Click(object sender, EventArgs e)
+        {
+            result = openFileDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                tbOldName.Text = getProjectName(openFileDialog.FileName);
+                btnRename.Enabled = true;
+            }
+        }
+        private void btnRename_Click(object sender, EventArgs e)
+        {
+            justDoIt();
+        }
+        private void tbNewName_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                justDoIt();
+            }
+        }
+        private void justDoIt()
+        {
+            if (tbOldName.Text == tbNewName.Text || tbNewName.Text.Length == 0) MessageBox.Show("Incorrect name");
+            else
+            {
+                DirectoryInfo pathDI = new DirectoryInfo(getDir(openFileDialog.FileName));
+                string path = pathDI.FullName.Replace(tbOldName.Text, tbNewName.Text);
+                Directory.Move(pathDI.FullName, pathDI.FullName.Replace(tbOldName.Text, tbNewName.Text));
+                RenameDirs(path);
+                RenameFiles(path);
+                RenameInFiles(path);
+                btnRename.Enabled = false;
+                MessageBox.Show("Old name: " + tbOldName.Text + Environment.NewLine + "New name: " + tbNewName.Text, "Replaced successfully");
+            }
+        }
+        private void RenameFiles(string v)
+        {
+            p = new DirectoryInfo(v);
+            foreach (FileInfo child in p.GetFiles("*", SearchOption.AllDirectories))
+            {
+                string newName = child.FullName.Replace(tbOldName.Text, tbNewName.Text);
+
+                if (newName != child.FullName)
+                {
+                    try
+                    {
+                        child.MoveTo(newName);
+                    }
+                    catch (Exception) { }
+                }
+            }
+        }
+        private void RenameInFiles(string v)
+        {
+            p = new DirectoryInfo(v);
+            
+            foreach (var file in p.GetFiles("*", SearchOption.AllDirectories))
+            {
+                info = new FileInfo(file.FullName);
+                try
+                {
+                    File.WriteAllText(info.ToString(), File.ReadAllText(info.ToString()).Replace(tbOldName.Text, tbNewName.Text));
+                }
+                catch (Exception) { }
+            }
+        }
+        private void RenameDirs(string v)
+        {
+            p = new DirectoryInfo(v);
+            foreach (DirectoryInfo child in p.GetDirectories("*", SearchOption.AllDirectories))
+            {
+                string newName = child.FullName.Replace(tbOldName.Text, tbNewName.Text);
+                if (newName != child.FullName)
+                {
+                    try
+                    {
+                        child.MoveTo(newName);
+                    }
+                    catch (Exception) { }
+                }
+            }
+        }
+        private string getDir(string fileName)
+        {
+            st.Clear();
+            s = fileName.Split('\\');
+            for (int i = 0; i < s.Length - 1; i++)
+            {
+                st.Append(s[i] + "\\");
+            }
+            return st.ToString();
+        }
+        private string getProjectName(string selectedPath)
+        {
+            s = selectedPath.Split('\\');
+            return s[s.Length - 1].Replace(".sln","");
+        }
+    }
+}
